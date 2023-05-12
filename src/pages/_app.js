ef8f14e9af3676d1ebd8a4828583716925de5f01
Steven Medina <smedina2494@gmail.com>
2023-05-11 22:07:28 -0400

    Sets up new project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Montserrat } from 'next/font/google';
+import Head from 'next/head';
+
+const monsterrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-monsterrat',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+              <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <main className={`${monsterrat.variable} font-monsterrat bg-light`}>
+      <Component {...pageProps} />
+    </main>
+    </>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        // font family used
+        monsterrat: ['var(--font-monsterrat)', ...fontFamily.sans],
+      },
+      colors: {
+        // colors used
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        primary: '#B63E96', // 240,86,199
+        primaryDark: '#58E6D9', // 80,230,217
+      },
+    },
+  },
+  plugins: [],
+};
