ab84021a57e34f7408b3d14ad2e6dc32d91f5a12
Steven Medina <smedina2494@gmail.com>
2023-05-14 15:02:26 -0400

    Fixes apostraphy problem

--- src/pages/about.js
+++ src/pages/about.js
@@ -49,20 +49,20 @@ const about = () => {
                 Biography
               </h2>
               <p className='font-medium'>
-                Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+                Hi, I am Steven, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 4 years of experience in the field. I
-                am always looking for new and innovative ways to bring my
-                clients' visions to life.
+                am always looking for new and innovative ways to bring visions
+                to life.
               </p>
 
               <p className='my-4 font-medium'>
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty – it is about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
               <p className='font-medium'>
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I am working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
