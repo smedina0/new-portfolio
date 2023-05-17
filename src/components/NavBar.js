c8058b25182c2769e3407c2dc5994d1e525366ee
Steven Medina <smedina2494@gmail.com>
2023-05-16 21:37:05 -0400

    Tests a tag responsive fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -280,7 +280,7 @@ const NavBar = () => {
                 whileHover={{ y: -2 }}
                 whileTap={{ scale: 0.9 }}
               >
-                <button
+                <a
                   onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
                   className={`ml-3 flex items-center justify-center rounded-full p-1
     ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}
@@ -292,7 +292,7 @@ const NavBar = () => {
                   ) : (
                     <MoonIcon className={'fill-dark'} />
                   )}
-                </button>
+                </a>
               </motion.li>
             </ul>
           </nav>
