e1c0e8c3617fe1017b3ae161fec03c4bffb4ba46
Steven Medina <smedina2494@gmail.com>
2023-05-20 00:27:10 -0400

    FIX: Fixes resume button

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,10 +44,10 @@ export default function Home() {
                 <a
                   href='/Medina SEI Resume.pdf'
                   target={'_blank'}
-                  className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:py-2 md:text-base sm:text-base sm:p-2 sm:px-4 sm:py-2 xs:p-2 xs:px-4 xs:text-base '
+                  className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:py-2 md:text-base sm:text-base sm:p-2 sm:px-4 sm:py-2 xs:p-2 xs:px-4 xs:text-base'
                   download={true}
                 >
-                  Resume <LinkArrow className={'w-6 ml-1'} />
+                  Resume <LinkArrow className={'w-6 ml-1 sm:h-6 sm:w-6'} />
                 </a>
                 <Link
                   href='https://www.linkedin.com/in/smedinaa11y/'
